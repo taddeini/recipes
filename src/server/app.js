52a54c0e4c9a534344884cbd3abac5a41f552585
ataddeini <ataddeini@gmail.com>
2016-05-03 20:10:23 -0500

    webpack updates

--- src/server/app.js
+++ src/server/app.js
@@ -1,24 +1,12 @@
 var express = require('express');
-var path = require('path');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-var favicon = require('serve-favicon');
-var expressHandlebars = require('express-handlebars');
 
 module.exports = function(env) {
   var app = express();
 
-  app.set('views', path.join(__dirname, './views'));
-  app.engine('handlebars', expressHandlebars());
-  app.set('view engine', 'handlebars');
-  //app.use(favicon(path.join(__dirname, '../../dist/favicon.ico')));
   app.use(compression());
   app.use(bodyParser.json());
-
-  // Routes
-  app.use('/', express.static(path.join(__dirname, '../../dist')));
-  
-  require('./controllers/index')(app);
 
   return app;
 };
